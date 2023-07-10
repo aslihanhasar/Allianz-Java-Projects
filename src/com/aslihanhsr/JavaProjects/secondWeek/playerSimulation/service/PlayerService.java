9293ffd9ad6bc837b137dc6179f0c5e35696c57a
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-11 00:01:55 +0300

    Add PlayerService methods

--- src/com/aslihanhsr/JavaProjects/secondWeek/playerSimulation/service/PlayerService.java
+++ src/com/aslihanhsr/JavaProjects/secondWeek/playerSimulation/service/PlayerService.java
@@ -1,5 +1,35 @@
 package com.aslihanhsr.JavaProjects.secondWeek.playerSimulation.service;
 
+import com.aslihanhsr.JavaProjects.secondWeek.playerSimulation.model.Player;
+import com.aslihanhsr.JavaProjects.secondWeek.playerSimulation.model.Team;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerService {
 
+    public Player createPlayer(String name, String surname, int kitNumber, int birthYear,
+                               String position, BigDecimal value) {
+
+        Player player = new Player();
+        player.setName(name);
+        player.setSurname(surname);
+        player.setKitNumber(kitNumber);
+        player.setBirthYear(birthYear);
+        player.setPosition(position);
+        player.setValue(value);
+
+        return player;
+    }
+
+    public void addTeamToPlayer(Player player, Team team){
+        if(player.getTeamList()!=null){
+            player.getTeamList().add(team);
+        }else{
+            List<Team> teamList=new ArrayList<>();
+            teamList.add(team);
+            player.setTeamList(teamList);
+        }
+    }
 }
