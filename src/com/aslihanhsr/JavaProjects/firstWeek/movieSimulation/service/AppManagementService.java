4ae23ccbe2caac12a4d901e1532fc84722027a2d
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-10 20:58:51 +0300

    Fix bugs

--- src/com/aslihanhsr/JavaProjects/firstWeek/movieSimulation/service/AppManagementService.java
+++ src/com/aslihanhsr/JavaProjects/firstWeek/movieSimulation/service/AppManagementService.java
@@ -38,93 +38,96 @@ public class AppManagementService {
 
     public void listMoviesByCategoryName(String categoryName) {
         Category category = getCategoryByName(categoryName);
-        for (Movie movie : movies) {
-            boolean categoryExist = movie.getCategories().contains(category);
-            if (categoryExist) {
+        if (category != null) {
+            System.out.println(category);
+            for (Movie movie : movies) {
+                boolean categoryExist = movie.getCategories().contains(category);
+                if (categoryExist) {
+                    System.out.println(movie);
+                }
+            }
+        }else{
+            System.out.println("No such category found"+categoryName);
+        }
+    }
+        public void listCategories () {
+            for (Category category : categories) {
                 System.out.println(category);
+            }
+        }
+
+        public void listPlatforms () {
+            for (Platform platform : platforms) {
+                System.out.println(platform);
+            }
+        }
+
+        public void listMovies () {
+            for (Movie movie : movies) {
                 System.out.println(movie);
             }
         }
-    }
 
-    public void listCategories() {
-        for (Category category : categories) {
-            System.out.println(category);
+        private boolean addCategoriesToMovie (Movie movie, String[]categoriesInputArr){
+            List<Category> movieCategories = new ArrayList<>();
+            for (String categoryInput : categoriesInputArr) {
+                Category category = getCategoryByName(categoryInput);
+                if (category != null) {
+                    movieCategories.add(category);
+                } else {
+                    System.out.println("No such a category found. " + categoryInput);
+                    return false;
+                }
+            }
+            movie.setCategories(movieCategories);
+            return true;
         }
-    }
 
-    public void listPlatforms() {
-        for (Platform platform : platforms) {
-            System.out.println(platform);
+        private boolean addPlatformsToMovie (Movie movie, String[]platformsInputArr){
+            List<Platform> moviePlatforms = new ArrayList<>();
+            for (String platformInput : platformsInputArr) {
+                Platform platform = getPlatformByName(platformInput);
+                if (platform != null) {
+                    moviePlatforms.add(platform);
+                } else {
+                    System.out.println("No such a platform found. " + platformInput);
+                    return false;
+                }
+            }
+            movie.setPlatforms(moviePlatforms);
+            return true;
         }
-    }
 
-    public void listMovies() {
-        for (Movie movie : movies) {
-            System.out.println(movie);
+        private Category getCategoryByName (String categoryName){
+            for (Category category : categories) {
+                boolean categoriesMatch = category.getCategoryName().equalsIgnoreCase(categoryName);
+                if (categoriesMatch) {
+                    return category;
+                }
+            }
+            return null;
         }
-    }
 
-    private boolean addCategoriesToMovie(Movie movie, String[] categoriesInputArr) {
-        for (String categoryInput : categoriesInputArr) {
-            Category category = getCategoryByName(categoryInput);
-            if (category != null) {
-                List<Category> movieCategories = new ArrayList<>();
-                movieCategories.add(category);
-                movie.setCategories(movieCategories);
-            } else {
-                System.out.println("No such a category found. " + categoryInput);
-                return false;
+        private Platform getPlatformByName (String platformName){
+            for (Platform platform : platforms) {
+                boolean platformsMatch = platform.getPlatformName().equalsIgnoreCase(platformName);
+                if (platformsMatch) {
+                    return platform;
+                }
+            }
+            return null;
+        }
+
+        private void increaseMovieCounter (List < Category > movieCategories) {
+            for (Category movieCategory : movieCategories) {
+                String movieCategoryName = movieCategory.getCategoryName();
+                Category categoryByName = getCategoryByName(movieCategoryName);
+                if (categoryByName != null) {
+                    movieCategory.incrementMovieCounter();
+                } else {
+                    System.out.println("Category not found.");
+                }
             }
         }
-        return true;
-    }
 
-    private boolean addPlatformsToMovie(Movie movie, String[] platformsInputArr) {
-        for (String platformInput : platformsInputArr) {
-            Platform platform = getPlatformByName(platformInput);
-            if (platform != null) {
-                List<Platform> moviePlatforms = new ArrayList<>();
-                moviePlatforms.add(platform);
-                movie.setPlatforms(moviePlatforms);
-            } else {
-                System.out.println("No such a platform found. " + platformInput);
-                return false;
-            }
-        }
-        return true;
     }
-
-    private Category getCategoryByName(String categoryName) {
-        for (Category category : categories) {
-            boolean categoriesMatch = category.getCategoryName().equalsIgnoreCase(categoryName);
-            if (categoriesMatch) {
-                return category;
-            }
-        }
-        return null;
-    }
-
-    private Platform getPlatformByName(String platformName) {
-        for (Platform platform : platforms) {
-            boolean platformsMatch = platform.getPlatformName().equalsIgnoreCase(platformName);
-            if (platformsMatch) {
-                return platform;
-            }
-        }
-        return null;
-    }
-
-    private void increaseMovieCounter(List<Category> movieCategories) {
-        for (Category movieCategory : movieCategories) {
-            String movieCategoryName = movieCategory.getCategoryName();
-            Category categoryByName = getCategoryByName(movieCategoryName);
-            if (categoryByName != null) {
-                movieCategory.incrementMovieCounter();
-            } else {
-                System.out.println("Category not found.");
-            }
-        }
-    }
-
-}
