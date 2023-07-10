735161526dc0a956584bf704d277a447d9c3c8c0
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-11 02:44:41 +0300

    Update makeTransfer() method

--- src/com/aslihanhsr/JavaProjects/secondWeek/playerSimulation/service/TransferService.java
+++ src/com/aslihanhsr/JavaProjects/secondWeek/playerSimulation/service/TransferService.java
@@ -2,14 +2,34 @@ package com.aslihanhsr.JavaProjects.secondWeek.playerSimulation.service;
 
 import com.aslihanhsr.JavaProjects.secondWeek.playerSimulation.model.*;
 
+import java.math.BigDecimal;
+
 public class TransferService {
+    CurrencyService currencyService=new CurrencyService();
     public void makeTransfer(Player player, Team toTeam){
-        if(toTeam.getBudget().compareTo(player.getValue())>=0){
-            System.out.println("transfer can be done");
-            toTeam.setBudget(toTeam.getBudget().subtract(player.getValue()));
-            Team fromTeam=player.getTeamList().get(player.getTeamList().size()-1);
-            fromTeam.setBudget(fromTeam.getBudget().add(player.getValue()));
+        Team fromTeam = player.getTeamList().get(player.getTeamList().size() - 1);
+        Currency currencyFromTeam=fromTeam.getCurrency();
+        Currency currencyToTeam=toTeam.getCurrency();
+        Currency currencyPlayer=player.getCurrency();
+        BigDecimal budgetFromTeam=currencyService.makeConversionCurrency(fromTeam.getBudget(),
+                currencyFromTeam,currencyPlayer);
+        BigDecimal budgetToTeam=currencyService.makeConversionCurrency(toTeam.getBudget(),
+                currencyToTeam, currencyPlayer);
+
+        int compareResult=toTeam.getBudget().compareTo(player.getValue());
+        if(compareResult>=0){
+            System.out.println("Transfer can be done");
+            budgetFromTeam=fromTeam.getBudget().add(player.getValue());
+            budgetToTeam=toTeam.getBudget().subtract(player.getValue());
+
+            BigDecimal originalCurrencyFromTeam=currencyService.makeConversionCurrency(budgetFromTeam,
+                    currencyPlayer,currencyFromTeam);
+            BigDecimal originalCurrencyToTeam=currencyService.makeConversionCurrency(budgetToTeam,
+                    currencyPlayer,currencyToTeam);
+            fromTeam.setBudget(originalCurrencyFromTeam);
+            toTeam.setBudget(originalCurrencyToTeam);
             player.getTeamList().add(toTeam);
+
         }else{
             System.err.println("transfer can not be done because of not enough budget");
         }
