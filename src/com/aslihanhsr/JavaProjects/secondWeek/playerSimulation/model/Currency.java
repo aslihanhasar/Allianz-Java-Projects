ecd710ba7bb026662bf391a0288ec24e72bc02c8
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-11 01:21:28 +0300

    Add program codes via upload

--- src/com/aslihanhsr/JavaProjects/secondWeek/playerSimulation/model/Currency.java
+++ src/com/aslihanhsr/JavaProjects/secondWeek/playerSimulation/model/Currency.java
@@ -1,4 +1,24 @@
 package com.aslihanhsr.JavaProjects.secondWeek.playerSimulation.model;
 
 public class Currency {
+    private String currency;
+
+    public Currency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "currency='" + currency + '\'' +
+                '}';
+    }
 }
