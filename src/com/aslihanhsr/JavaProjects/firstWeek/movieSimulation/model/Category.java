454f83eeee9d678fb85c27e528acf0bff311e91e
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-10 17:26:25 +0300

    Add program codes via upload

--- src/com/aslihanhsr/JavaProjects/firstWeek/movieSimulation/model/Category.java
+++ src/com/aslihanhsr/JavaProjects/firstWeek/movieSimulation/model/Category.java
@@ -1,4 +1,32 @@
 package com.aslihanhsr.JavaProjects.firstWeek.movieSimulation.model;
 
 public class Category {
+    private final String categoryName;
+    private int movieCounter;
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public int getMovieCounter() {
+        return movieCounter;
+    }
+
+    public void setMovieCounter(int movieCounter) {
+        this.movieCounter = movieCounter;
+    }
+
+    public void incrementMovieCounter() {
+        this.movieCounter++;
+    }
+
+    @Override
+    public String toString() {
+        return "\nCategory Name: " + categoryName + '\n' +
+                "Number of Movies: " + movieCounter;
+    }
 }
