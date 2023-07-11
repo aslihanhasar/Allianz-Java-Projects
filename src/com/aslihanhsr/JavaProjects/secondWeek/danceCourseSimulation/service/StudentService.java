d70cb277d798b0f6e024ca9880e1348ff88f4ac4
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-12 01:07:12 +0300

    Add a create method for student

--- src/com/aslihanhsr/JavaProjects/secondWeek/danceCourseSimulation/service/StudentService.java
+++ src/com/aslihanhsr/JavaProjects/secondWeek/danceCourseSimulation/service/StudentService.java
@@ -1,4 +1,21 @@
 package com.aslihanhsr.JavaProjects.secondWeek.danceCourseSimulation.service;
 
+import com.aslihanhsr.JavaProjects.secondWeek.danceCourseSimulation.model.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class StudentService {
+    public Student createStudent(String name, int age, Sex sex, boolean isPaid,
+                                 BigDecimal contractAmount, Date startDate, Date endDate) {
+        Student student = new Student();
+        student.setName(name);
+        student.setAge(age);
+        student.setSex(sex);
+        student.setPaid(isPaid);
+        student.setContractAmount(contractAmount);
+        student.setStartDate(startDate);
+        student.setEndDate(endDate);
+        return student;
+    }
 }
