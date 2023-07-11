db876e4513181eaf83f977b95cb7432fb7dfba0b
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-12 00:54:54 +0300

    Add a create instructor method

--- src/com/aslihanhsr/JavaProjects/secondWeek/danceCourseSimulation/service/InstructorService.java
+++ src/com/aslihanhsr/JavaProjects/secondWeek/danceCourseSimulation/service/InstructorService.java
@@ -1,4 +1,21 @@
 package com.aslihanhsr.JavaProjects.secondWeek.danceCourseSimulation.service;
 
+import com.aslihanhsr.JavaProjects.secondWeek.danceCourseSimulation.model.Branch;
+import com.aslihanhsr.JavaProjects.secondWeek.danceCourseSimulation.model.Instructor;
+import com.aslihanhsr.JavaProjects.secondWeek.danceCourseSimulation.model.Sex;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class InstructorService {
+    public Instructor createInstructor(String name, List<Branch> branches, int age,
+                                       Sex sex, BigDecimal salary) {
+        Instructor instructor = new Instructor();
+        instructor.setName(name);
+        instructor.setBranches(branches);
+        instructor.setAge(age);
+        instructor.setSex(sex);
+        instructor.setSalary(salary);
+        return instructor;
+    }
 }
