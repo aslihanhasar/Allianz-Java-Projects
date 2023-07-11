52c514c922bae628e2815abb9964890c4ea93010
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-12 00:36:19 +0300

    Add days name

---
+++ src/com/aslihanhsr/JavaProjects/secondWeek/danceCourseSimulation/model/Day.java
@@ -0,0 +1,12 @@
+package com.aslihanhsr.JavaProjects.secondWeek.danceCourseSimulation.model;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+}
