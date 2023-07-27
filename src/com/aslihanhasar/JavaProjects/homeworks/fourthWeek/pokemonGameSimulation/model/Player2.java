1d893a22c2708ec384a48b57fc07e0472f15ea9a
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-28 02:50:56 +0300

    Add inheritance, extends Player.java

---
+++ src/com/aslihanhasar/JavaProjects/homeworks/fourthWeek/pokemonGameSimulation/model/Player2.java
@@ -0,0 +1,8 @@
+package com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation.model;
+
+public class Player2 extends Player{
+
+    public Player2(String name) {
+        super(2,name);
+    }
+}
