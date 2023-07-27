1bf3a5409b1aa6a32ba9cf44a87c3d1af8474ce5
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-27 20:02:55 +0300

    Create StrategyPower.java and SuperPower.java

--- src/com/aslihanhasar/JavaProjects/homeworks/fourthWeek/pokemonGameSimulation/model/StrategyPower.java
+++ src/com/aslihanhasar/JavaProjects/homeworks/fourthWeek/pokemonGameSimulation/model/StrategyPower.java
@@ -1,7 +1,8 @@
 package com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation.model;
 
-public class SuperPower extends Power{
-    public SuperPower(String name, int damage, int remainRight, PowerType powerType) {
+public class StrategyPower extends Power {
+
+    public StrategyPower(String name, int damage, int remainRight, PowerType powerType) {
         super(name, damage, remainRight, powerType);
     }
 }
