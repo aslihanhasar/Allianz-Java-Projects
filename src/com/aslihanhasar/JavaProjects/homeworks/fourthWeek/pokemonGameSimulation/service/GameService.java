066f18e1b519c3ecc08edc6eadd9a09ad925655d
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-28 14:04:31 +0300

    Create selectPokemon() and selectCharacter()

--- src/com/aslihanhasar/JavaProjects/homeworks/fourthWeek/pokemonGameSimulation/service/GameService.java
+++ src/com/aslihanhasar/JavaProjects/homeworks/fourthWeek/pokemonGameSimulation/service/GameService.java
@@ -1,15 +1,14 @@
 package com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation.service;
 
-import com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation.model.Game;
-import com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation.model.Player;
-import com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation.model.Player1;
+import com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation.model.*;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class GameService {
     private final Scanner scanner = new Scanner(System.in);
     private final PlayerService playerService = new PlayerService();
-    private final LoadService loadService=new LoadService();
+    private final LoadService loadService = new LoadService();
     private Game game;
     private String inputPlayer1;
     private String inputPlayer2;
@@ -18,33 +17,53 @@ public class GameService {
         boolean startGame = isGameStart();
         if (startGame) {
             System.out.println("******* The Pokemon Game is starting. *******");
-            Player player1=playerService.createPlayer(1);
-            Player player2=playerService.createPlayer(2);
-            game=new Game(player1,player2);
+            Player player1 = playerService.createPlayer(1);
+            Player player2 = playerService.createPlayer(2);
+            game = new Game(player1, player2);
             loadGame();
-
-
-
-
+            listCharactersInGame();
+            playerService.selectCharacter(game,player1);
+            playerService.selectCharacter(game,player2);
+            listPokemonsInGame();
+            playerService.selectPokemon(game,player1);
+            playerService.selectPokemon(game,player2);
+            System.out.println("Level 1 is starting..");
+            System.out.println("Player 1 Information: "+player1);
+            System.out.println("Player 2 Information: "+player2);
         } else {
             System.out.println("No player match found.");
         }
 
     }
 
-    private boolean isGameStart(){
+    private boolean isGameStart() {
         printStartMenu();
-        inputPlayer1 =scanner.next().trim();
+        inputPlayer1 = scanner.next().trim();
         printStartMenu();
-        inputPlayer2 =scanner.next().trim();
+        inputPlayer2 = scanner.next().trim();
         return inputPlayer1.equals(inputPlayer2);
     }
 
-    private void loadGame(){
+    private void loadGame() {
         System.out.println("Loading...");
         game.setGameCharacters(loadService.loadCharacters());
         game.setGamePokemons(loadService.loadPokemons());
     }
+
+    private void listCharactersInGame() {
+        System.out.println(" ****** CHARACTERS ******");
+        for (GameCharacter character : game.getGameCharacters()) {
+            System.out.println(character);
+        }
+    }
+
+    private void listPokemonsInGame() {
+        System.out.println(" ****** POKEMONS ******");
+        for (Pokemon pokemon : game.getGamePokemons()) {
+            System.out.println(pokemon);
+        }
+    }
+
     private void printStartMenu() {
         System.out.print("""
                 *********** THE POKEMON GAME ***********
--- src/com/aslihanhasar/JavaProjects/homeworks/fourthWeek/pokemonGameSimulation/service/PlayerService.java
+++ src/com/aslihanhasar/JavaProjects/homeworks/fourthWeek/pokemonGameSimulation/service/PlayerService.java
@@ -2,18 +2,24 @@ package com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation
 
 import com.aslihanhasar.JavaProjects.homeworks.fourthWeek.pokemonGameSimulation.model.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PlayerService {
     private final Scanner scanner;
+    private final GameCharacterService characterService;
+    private final PokemonService pokemonService;
 
-    public PlayerService(){
-        scanner=new Scanner(System.in);
+    public PlayerService() {
+        scanner = new Scanner(System.in);
+        characterService=new GameCharacterService();
+        pokemonService=new PokemonService();
     }
 
-    public Player createPlayer(int id){
+    public Player createPlayer(int id) {
         System.out.println("Create player. Enter the name: ");
-        String name=scanner.next();;
+        String name = scanner.next();
         switch (id) {
             case 1 -> {
                 return new Player1(name);
@@ -22,9 +28,62 @@ public class PlayerService {
                 return new Player2(name);
             }
             default -> {
+                System.out.println("Error! Failed to create player");
                 return null;
             }
         }
+    }
+
+    public void selectCharacter(Game game, Player player) {
+        System.out.print("Enter the id of the character you want to select: ");
+        int characterId = scanner.nextInt();
+        GameCharacter gameCharacter = getGameCharacterById(game, characterId);
+        if (gameCharacter != null) {
+            int playerId = player.getId();
+            switch (playerId) {
+                case 1 -> game.getPlayer1().setGameCharacter(gameCharacter);
+                case 2 -> game.getPlayer2().setGameCharacter(gameCharacter);
+                default -> System.out.println("An error occurred while selecting a character.");
+            }
+        } else {
+            System.out.println("There is no such character");
+        }
+    }
+
+    public void selectPokemon(Game game, Player player) {
+        System.out.print("Enter the id of the pokemon you want to select: ");
+        int pokemonId = scanner.nextInt();
+        Pokemon pokemon=getGamePokemonById(game,pokemonId);
+        if(pokemon!=null){
+            int playerId=player.getId();
+            switch (playerId){
+                case 1-> game.getPlayer1().getGameCharacter().getPokemons().add(pokemon);
+                case 2-> game.getPlayer2().getGameCharacter().getPokemons().add(pokemon);
+                default -> System.out.println("An error occurred while selecting a pokemon.");
+            }
+        }else{
+            System.out.println("There is no such character");
+        }
+    }
+
+    private Pokemon getGamePokemonById(Game game, int id) {
+        List<Pokemon> gamePokemons = game.getGamePokemons();
+        for (Pokemon pokemon : gamePokemons) {
+            if (pokemon.getId() == id) {
+                return pokemon;
+            }
+        }
+        return null;
+    }
+
+    private GameCharacter getGameCharacterById(Game game, int id) {
+        List<GameCharacter> gameCharacters = game.getGameCharacters();
+        for (GameCharacter gameCharacter : gameCharacters) {
+            if (gameCharacter.getId() == id) {
+                return gameCharacter;
+            }
+        }
+        return null;
     }
 
 }
