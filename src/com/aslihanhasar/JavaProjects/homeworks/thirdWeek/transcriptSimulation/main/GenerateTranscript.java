05189a7cecb62adba80c4edcc6a06a4bf54aa667
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-30 15:24:39 +0300

    Create takeInputFromUser()

--- src/com/aslihanhasar/JavaProjects/homeworks/thirdWeek/transcriptSimulation/main/GenerateTranscript.java
+++ src/com/aslihanhasar/JavaProjects/homeworks/thirdWeek/transcriptSimulation/main/GenerateTranscript.java
@@ -1,5 +1,9 @@
 package com.aslihanhasar.JavaProjects.homeworks.thirdWeek.transcriptSimulation.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -26,11 +30,42 @@ public class GenerateTranscript {
                 CourseGrade courseGrade = new CourseGrade(courseDepartment, courseCode, courseCredit);
                 courseGrade.setGradeTaken(gradeTaken);
                 transcript.addCourseTaken(courseGrade);
-            }catch (NoSuchElementException e){
+            } catch (NoSuchElementException e) {
                 System.out.println("Generating transcript...");
             }
         }
         System.out.println(transcript);
     }
+
+    public void takeInputFromFile() {
+        try {
+            System.out.print("Enter the file name: ");
+            String fileName = "src/com/aslihanhasar/JavaProjects/homeworks/thirdWeek/" +
+                    "transcriptSimulation/" + scanner.next().trim();
+            File file = new File(fileName);
+            Scanner fileScanner = new Scanner(file);
+            int studentID = fileScanner.nextInt();
+            fileScanner.nextLine();
+            Transcript transcript = new Transcript(studentID);
+            while (fileScanner.hasNextLine()) {
+                String courseDepartment = fileScanner.next();
+                int courseCode = fileScanner.nextInt();
+                int courseCredit = fileScanner.nextInt();
+                double gradeTaken = fileScanner.nextDouble();
+                fileScanner.nextLine();
+                CourseGrade courseGrade = new CourseGrade(courseDepartment, courseCode, courseCredit);
+                courseGrade.setGradeTaken(gradeTaken);
+                transcript.addCourseTaken(courseGrade);
+            }
+            System.out.println(transcript);
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: File not found.");
+        } catch (Exception e) {
+            System.err.println("Error: Invalid file format." + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
+
 
